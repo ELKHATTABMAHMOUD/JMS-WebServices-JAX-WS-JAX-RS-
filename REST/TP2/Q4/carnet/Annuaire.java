0905d521f550a268f5a85e9e259b70442353c2e0
EL KHATTAB Mahmoud <elkhattab.mahmoud@gmail.com>
2018-04-05 15:14:12 +0200

    rest jersey - question 4 completed

---
+++ REST/TP2/Q4/carnet/Annuaire.java
@@ -0,0 +1,48 @@
+package carnet ;
+
+import javax.ws.rs.* ;
+
+
+@Path("/carnet")
+public class Annuaire{
+	
+	private static Carnet carnet = new Carnet() ; 
+	
+	@GET
+	@Produces("text/html")
+	public String allContacts(){
+		if(carnet.getContacts().size() == 0)
+			return "Liste Vide" ;
+		return carnet.toString();	
+		
+	}
+	@Path("/{name}")
+	@GET
+	@Produces("text/html")
+	public String getContact(@PathParam("name") String name){
+		for(Contact contact : this.carnet.getContacts()){
+			if(contact.getNom().equals(name))
+				return contact.getNumero();
+		}
+		return "Inconnu";	
+		
+	}
+	
+	@Path("/addContact")
+	@POST
+	public String addContact(@FormParam("name")String name, @FormParam("number") String numero){
+		
+		for(Contact contact : this.carnet.getContacts()){
+			if(contact.getNom().equals(name))
+				return "Contact ... déjà existant";
+		}
+		if(!name.equals("") && !numero.equals("")){
+			Contact newContact = new Contact(name, numero);
+			carnet.getContacts().add(newContact);
+			return "Contact ... créé";
+		}
+		return "Vous n'avez pas renseigné numero ou nom." ;
+ 
+	}
+	
+}
